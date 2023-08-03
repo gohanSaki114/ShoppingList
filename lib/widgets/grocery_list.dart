e29f91db3e397ba7e7f2fc3b421e20c92faba400
Sagar Khati <sagar.khati@1rivet.com>
2023-08-03 18:21:34 +0530

    first commit

---
+++ lib/widgets/grocery_list.dart
@@ -0,0 +1,105 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
+import 'package:shopping_list/models/grocery_item.dart';
+import 'package:shopping_list/widgets/new_item.dart';
+import 'package:http/http.dart' as http;
+
+class GroceyList extends StatefulWidget {
+  const GroceyList({super.key});
+
+  @override
+  State<GroceyList> createState() => _GroceyListState();
+}
+
+class _GroceyListState extends State<GroceyList> {
+  List<GroceryItem> _groceryItems = [];
+  var _isLoading = true;
+  @override
+  void initState() {
+    super.initState();
+    _loadItems();
+  }
+
+  void _loadItems() async {
+    final url = Uri.https(
+        'flutter-prep-84057-default-rtdb.firebaseio.com', 'shopping-list.json');
+    final response = await http.get(url);
+    final Map<String, dynamic> listData = json.decode(response.body);
+    final List<GroceryItem> _loadedItems = [];
+    for (final item in listData.entries) {
+      final category = categories.entries
+          .firstWhere(
+              (firstItem) => firstItem.value.title == item.value['category'])
+          .value;
+      _loadedItems.add(GroceryItem(
+          id: item.key,
+          name: item.value['name'],
+          quantity: item.value['quantity'],
+          category: category));
+    }
+    setState(() {
+      _groceryItems = _loadedItems;
+      _isLoading = false;
+    });
+  }
+
+  void _addItem() async {
+    final newItem = await Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => const NewItem(),
+    ));
+
+    if (newItem == null) {
+      return;
+    }
+
+    setState(() {
+      _groceryItems.add(newItem);
+    });
+  }
+
+  void _removeItem(GroceryItem item) {
+    setState(() {
+      _groceryItems.remove(item);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = const Center(child: Text('No items added yet.'));
+    if(_isLoading)
+    {
+      content = const Center(child: CircularProgressIndicator(),);
+    }
+
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (context, index) => Dismissible(
+          onDismissed: (direction) {
+            _removeItem(_groceryItems[index]);
+          },
+          key: ValueKey(_groceryItems[index].id),
+          child: ListTile(
+            title: Text(_groceryItems[index].name),
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: _groceryItems[index].category.color,
+            ),
+            trailing: Text(_groceryItems[index].quantity.toString()),
+          ),
+        ),
+      );
+    }
+
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Your Groceries'),
+          actions: [
+            IconButton(onPressed: _addItem, icon: const Icon(Icons.add)),
+          ],
+        ),
+        body: content);
+  }
+}
